4daa22e8d3f3ae3668c74c8f06b9e9603acc479b
Josh K Preston <kanemetal@gmail.com>
2020-10-09 13:56:19 -0600

    'init'

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+// declare variables
+let item = 0
+
+
+
+// gather elements from DOM
+let target = document.getElementById('target')
+
+
+
+// functions
+let clickTarget = target => {
+  item++
+}
+
+let draw = () => {
+  
+}
+
+
+// calls
+draw()
