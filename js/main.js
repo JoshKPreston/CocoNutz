7742e94ae79cb9e429ef730654b5caee38db920e
Josh K Preston <kanemetal@gmail.com>
2020-10-12 04:54:23 -0600

    final push

--- js/main.js
+++ js/main.js
@@ -1,44 +1,64 @@
 
-
-// declare global variables
-let coconutCount = 0
-let staticModMultiplier = 0
-let autoModMultiplier = 0
-let autoModIntervalsArray = []
-let modAvailabilityIntervalsArray = []
-let intervalName = null
-let priceIncrementValue = 0.15
-
 // classes
 class Modifier {
-  constructor(name, price, multiplier, isAuto, seconds) {
+  constructor(name, price, multiplier, isAuto) {
     this.name = name
     this.price = price
     this.multiplier = multiplier
     this.inventory = 0
-    this.isAvailable = false
     this.isAuto = isAuto
-    if (this.isAuto) {
-      this.autoInterval = 1000;
-      this.seconds = seconds
-    }
+    this.isAvailable = false
   }
 }
 
-// objects
-/*
-30
-90
-270
-810
-*/
 let mods = {
-  machete: new Modifier('Machete', 1, 1, false),
-  toucan: new Modifier('Toucan', 1, 1, true, 1),
-  monkey: new Modifier('Monkey', 1, 10, true, 5),
-  troop: new Modifier('Troop', 1, 15, true, 3)
+  machete: new Modifier('Machete', 30, 1, false),
+  toucan: new Modifier('Toucan', 90, 1, true),
+  monkey: new Modifier('Monkey', 270, 10, true),
+  troop: new Modifier('Troop', 810, 15, true)
 }
 
+let modElems = {
+  machete: {
+    macheteBtnElem: document.getElementById('macheteBtn'),
+    machetePriceElem: document.getElementById('machetePrice'),
+    machetePriceDivElem: document.getElementById('machetePriceDiv'),
+    macheteCountElem: document.getElementById('macheteCount'),
+    macheteMultiplierElem: document.getElementById('macheteMultiplier'),
+    macheteOverlayElem: document.getElementById('macheteOverlay')
+  },
+  toucan: {
+    toucanBtnElem: document.getElementById('toucanBtn'),
+    toucanPriceElem: document.getElementById('toucanPrice'),
+    toucanPriceDivElem: document.getElementById('toucanPriceDiv'),
+    toucanCountElem: document.getElementById('toucanCount'),
+    toucanMultiplierElem: document.getElementById('toucanMultiplier'),
+    toucanOverlayElem: document.getElementById('toucanOverlay')
+  },
+  monkey: {
+    monkeyBtnElem: document.getElementById('monkeyBtn'),
+    monkeyPriceElem: document.getElementById('monkeyPrice'),
+    monkeyPriceDivElem: document.getElementById('monkeyPriceDiv'),
+    monkeyCountElem: document.getElementById('monkeyCount'),
+    monkeyMultiplierElem: document.getElementById('monkeyMultiplier'),
+    monkeyOverlayElem: document.getElementById('monkeyOverlay')
+  },
+  troop: {
+    troopBtnElem: document.getElementById('troopBtn'),
+    troopPriceElem: document.getElementById('troopPrice'),
+    troopPriceDivElem: document.getElementById('troopPriceDiv'),
+    troopCountElem: document.getElementById('troopCount'),
+    troopMultiplierElem: document.getElementById('troopMultiplier'),
+    troopOverlayElem: document.getElementById('troopOverlay')
+  }
+}
+
+// declare global variables
+let currentUser = {}
+let coconutCount = 0
+let staticModMultiplier = 0
+let autoModMultiplier = 0
+let priceIncrementValue = 0.15
 
 // gather elements from DOM
 let coconutCountElem = document.getElementById('coconutCount')
@@ -46,124 +66,152 @@ let targetElem = document.getElementById('target')
 let totalStaticModMultiplierElem = document.getElementById('totalStaticModMultiplier')
 let totalAutoModMultiplierElem = document.getElementById('totalAutoModMultiplier')
 let modifiersElem = document.getElementsByClassName('modifier')
-
-// machete
-let macheteBtnElem = document.getElementById('macheteBtn')
-let machetePriceElem = document.getElementById('machetePrice')
-let machetePriceDivElem = document.getElementById('machetePriceDiv')
-let macheteCountElem = document.getElementById('macheteCount')
-let macheteMultiplierElem = document.getElementById('macheteMultiplier')
-let macheteOverlayElem = document.getElementById('macheteOverlay')
-
-// toucan
-let toucanBtnElem = document.getElementById('toucanBtn')
-let toucanPriceElem = document.getElementById('toucanPrice')
-let toucanPriceDivElem = document.getElementById('toucanPriceDiv')
-let toucanCountElem = document.getElementById('toucanCount')
-let toucanMultiplierElem = document.getElementById('toucanMultiplier')
-let toucanOverlayElem = document.getElementById('toucanOverlay')
-
-// monkey
-let monkeyBtnElem = document.getElementById('monkeyBtn')
-let monkeyPriceElem = document.getElementById('monkeyPrice')
-let monkeyPriceDivElem = document.getElementById('monkeyPriceDiv')
-let monkeyCountElem = document.getElementById('monkeyCount')
-let monkeyMultiplierElem = document.getElementById('monkeyMultiplier')
-let monkeyOverlayElem = document.getElementById('monkeyOverlay')
-
-// troop
-let troopBtnElem = document.getElementById('troopBtn')
-let troopPriceElem = document.getElementById('troopPrice')
-let troopPriceDivElem = document.getElementById('troopPriceDiv')
-let troopCountElem = document.getElementById('troopCount')
-let troopMultiplierElem = document.getElementById('troopMultiplier')
-let troopOverlayElem = document.getElementById('troopOverlay')
-
-
+let messageBoxElem = document.getElementById('messageBox')
+let userFormElem = document.getElementById('userForm')
+let currentUserNameElem = document.getElementById('currentUserName')
+let topScoreElem = document.getElementById('topScore')
 
 // functions
 
 let hasEnoughCocos = mod => coconutCount >= mod.price ? true : false
-
-let addClass = (elem, className) => {
-  elem.classList.add(className)
-}
-
-let removeClass = (elem, className) => {
-  elem.classList.remove(className)
-}
-
-let checkModAvailability = mod => {
-  let elem = null
-  switch (mod.name.toString().toLowerCase()) {
-    case 'machete':
-      elem = {
-        overlay: macheteOverlayElem, 
-        button: macheteBtnElem,
-        price: machetePriceDivElem
-      }
-      break
-    case 'toucan':
-      elem = {
-        overlay: toucanOverlayElem, 
-        button: toucanBtnElem,
-        price: toucanPriceDivElem
-      }
-      break
-    case 'monkey':
-      elem = {
-        overlay: monkeyOverlayElem, 
-        button: monkeyBtnElem,
-        price: monkeyPriceDivElem
-      }
-      break
-    case 'troop':
-      elem = {
-        overlay: troopOverlayElem, 
-        button: troopBtnElem,
-        price: troopPriceDivElem
-      }
-      break
-    default:
-      console.log('checkModAvailability function did not find any matching case statement for switch (mod.name)')
-      break
+let setScores = () => {
+  coconutCount += autoModMultiplier
+  if (coconutCount > currentUser.topScore) {
+    currentUser.topScore = coconutCount
+    saveUsers()
   }
-  if (hasEnoughCocos(mod)) {
-      removeClass(elem.overlay, 'mod-not-available')
-      removeClass(elem.price, 'price-counter-red')
-      removeClass(elem.button, 'roll-in-left')
-      removeClass(elem.button, 'roll-in-top')
-      removeClass(elem.button, 'roll-in-right')
-      addClass(elem.button, 'vibrate-3')
-  } else {
-      addClass(elem.overlay, 'mod-not-available')
-      addClass(elem.price, 'price-counter-red')
-      removeClass(elem.button, 'vibrate-3')
-  }
+  draw()
 }
+let addClass = (elem, className) => elem.classList.add(className)
+let removeClass = (elem, className) => elem.classList.remove(className)
 
-let setCheckModAvailabilityInterval = miliseconds => {
+// intervals
+let autoGatherInterval = setInterval(setScores, 1000)
+
+let checkModAvailability = () => {
   for (const key in mods) {
     if (mods.hasOwnProperty(key)) {
       const mod = mods[key];
-      intervalName = mod.name.toString().toLowerCase()
-      modAvailabilityIntervalsArray[intervalName] = setInterval(checkModAvailability, miliseconds, mod)
+      let elem = null
+      switch (mod.name.toString().toLowerCase()) {
+        case 'machete':
+          elem = {
+            overlay: modElems.machete.macheteOverlayElem,
+            button: modElems.machete.macheteBtnElem,
+            price: modElems.machete.machetePriceDivElem,
+            inventory: modElems.machete.macheteCountElem
+          }
+          break
+        case 'toucan':
+          elem = {
+            overlay: modElems.toucan.toucanOverlayElem,
+            button: modElems.toucan.toucanBtnElem,
+            price: modElems.toucan.toucanPriceDivElem,
+            inventory: modElems.toucan.toucanCountElem
+          }
+          break
+        case 'monkey':
+          elem = {
+            overlay: modElems.monkey.monkeyOverlayElem,
+            button: modElems.monkey.monkeyBtnElem,
+            price: modElems.monkey.monkeyPriceDivElem,
+            inventory: modElems.monkey.monkeyCountElem
+          }
+          break
+        case 'troop':
+          elem = {
+            overlay: modElems.troop.troopOverlayElem,
+            button: modElems.troop.troopBtnElem,
+            price: modElems.troop.troopPriceDivElem,
+            inventory: modElems.troop.troopCountElem
+          }
+          break
+        default:
+          console.log('checkModAvailability function did not find any matching case statement for switch (mod.name)')
+          break
+      }
+
+      // check mod availability and make class changes accordingly
+      if (hasEnoughCocos(mod)) {
+        removeClass(elem.overlay, 'mod-not-available')
+        removeClass(elem.price, 'price-counter-red')
+        removeClass(elem.button, 'roll-in-left')
+        removeClass(elem.button, 'roll-in-bottom')
+        removeClass(elem.button, 'roll-in-top')
+        removeClass(elem.button, 'roll-in-right')
+        addClass(elem.button, 'vibrate-3')
+      } else {
+        removeClass(elem.button, 'vibrate-3')
+        addClass(elem.overlay, 'mod-not-available')
+        addClass(elem.price, 'price-counter-red')
+      }
+
+      // check inventory and make class changes accordingly
+      if (mod.inventory > 0) {
+        addClass(elem.inventory, 'inv-counter-green')
+      }
+
+      if (coconutCount >= 10000) { acheivementPopup() }
     }
   }
 }
+let checkModAvailabilityInterval = setInterval(checkModAvailability, 100)
 
-let setAutoModInterval = (mod, seconds) => {
-  intervalName = mod.name.toString().toLowerCase()
-  autoModIntervalsArray[intervalName] = setInterval(() => {
-    coconutCount += mod.multiplier; draw()
-  }, mod.autoInterval * seconds)
+
+// functions
+let popup1M = 0
+let acheivementPopup = () => {
+  if (!popup1M) {
+    messageBoxElem.innerHTML = /* html */ `
+    <div class="row h-100 justify-content-center align-items-center msg-box-row">
+      <div class="col-12 col-md-8 col-lg-6 msg-box-col">
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+          <div class="card-header">Achievement Unlocked!</div>
+          <div class="card-body">
+            <h5 class="card-title">+10,000 CocoNutz!</h5>
+            <p class="card-text">Somehow, you've managed to reach ultimate idleness! Some would call you nutz!<br>
+            <small>Your purchasable CocoBuffs have been quadrupled for 5 minutes!</small></p>
+          </div>
+        </div>
+      </div>
+    </div>  
+    `
+    messageBoxElem.classList.remove("hidden")
+    setTimeout(() => {
+      messageBoxElem.classList.add("hidden")
+    }, 10000);
+
+    mods.machete.multiplier = mods.machete.multiplier * 4
+    mods.toucan.multiplier = mods.toucan.multiplier * 4
+    mods.monkey.multiplier = mods.monkey.multiplier * 4
+    mods.troop.multiplier = mods.troop.multiplier * 4
+
+    addClass(modElems.machete.macheteMultiplierElem, 'inv-counter-green')
+    addClass(modElems.toucan.toucanMultiplierElem, 'inv-counter-green')
+    addClass(modElems.monkey.monkeyMultiplierElem, 'inv-counter-green')
+    addClass(modElems.troop.troopMultiplierElem, 'inv-counter-green')
+
+
+    setTimeout(() => {
+      mods.machete.multiplier = mods.machete.multiplier / 4
+      mods.toucan.multiplier = mods.toucan.multiplier / 4
+      mods.monkey.multiplier = mods.monkey.multiplier / 4
+      mods.troop.multiplier = mods.troop.multiplier / 4
+
+      removeClass(modElems.machete.macheteMultiplierElem, 'inv-counter-green')
+      removeClass(modElems.toucan.toucanMultiplierElem, 'inv-counter-green')
+      removeClass(modElems.monkey.monkeyMultiplierElem, 'inv-counter-green')
+      removeClass(modElems.troop.troopMultiplierElem, 'inv-counter-green')
+
+    }, 300000);
+
+    popup1M = 1
+  }
 }
 
 let sumMods = mod => {
   if (mod.isAuto) {
-    let seconds = mod.seconds
     autoModMultiplier += mod.multiplier
-    setAutoModInterval(mod, seconds)
   } else {
     staticModMultiplier += mod.multiplier
   }
@@ -174,8 +222,8 @@ let applyMod = strModName => {
   let mod = mods[strModName]
   if (hasEnoughCocos(mod)) {
     coconutCount -= mod.price
-    mod.inventory++
     mod.price += Math.ceil(mod.price * priceIncrementValue)
+    mod.inventory++
     sumMods(mod)
   }
 }
@@ -186,27 +234,98 @@ let gather = () => {
   draw()
 }
 
+
+let getSetUser = (event) => {
+  event.preventDefault()
+  let form = event.target
+  let userName = form.userName.value
+  currentUser = users.find(user => user.name == userName)
+  if (!currentUser) {
+    currentUser = {
+      name: userName,
+      topScore: 0
+    }
+    users.push(currentUser)
+  }
+  form.reset()
+  messageBoxElem.classList.add("hidden")
+  targetElem.classList.remove('hidden')
+  modElems.machete.macheteBtnElem.classList.remove('hidden')
+  modElems.toucan.toucanBtnElem.classList.remove('hidden')
+  modElems.monkey.monkeyBtnElem.classList.remove('hidden')
+  modElems.troop.troopBtnElem.classList.remove('hidden')
+  targetElem.classList.add('roll-in-top')
+  modElems.machete.macheteBtnElem.classList.add('roll-in-left')
+  modElems.toucan.toucanBtnElem.classList.add('roll-in-bottom')
+  modElems.monkey.monkeyBtnElem.classList.add('roll-in-top')
+  modElems.troop.troopBtnElem.classList.add('roll-in-right')
+  draw()
+}
+
+let saveUsers = () => window.localStorage.setItem("users", JSON.stringify(users))
+
+let users = []
+let loadUsers = () => {
+  let usersData = JSON.parse(window.localStorage.getItem("users"))
+  if (usersData) { users = usersData }
+}
+loadUsers()
+
+
+
+// templates
+
+$(document).ready(() => {
+  messageBoxElem.innerHTML = /* html */ `
+  <div class="row h-100 justify-content-center align-items-center msg-box-row">
+    <div class="col-12 col-md-8 col-lg-6 msg-box-col">
+
+      <form id="userForm" onsubmit="getSetUser(event)" class="form-group text-center msg-box-form">
+        <label for="userName"></label>
+        <input id="userName" type="text" class="form-control text-center" name="userName" 
+          aria-describedby="emailHelpId" placeholder="Enter Name">
+        <button id="userFormSubmit" type="submit" class="btn btn-danger btn-block">GO NUTZ!</button>
+      </form>
+        
+    </div>
+  </div>  
+  `
+
+  // prevent default action on form submit
+  const userForm = document.getElementById('userForm')
+
+  userForm.addEventListener('submit', (e) => {
+    e.preventDefault(); console.log('User signed in...')
+  })
+})
+
+
 let draw = () => {
   coconutCountElem.innerText = coconutCount.toString()
   totalStaticModMultiplierElem.innerText = staticModMultiplier.toString()
   totalAutoModMultiplierElem.innerText = autoModMultiplier.toString()
+  if (currentUser.name) {
+    currentUserNameElem.innerText = currentUser.name.toString()
+    topScoreElem.innerText = currentUser.topScore.toString()
+  }
 
-  machetePriceElem.innerText = mods.machete.price.toString()
-  macheteCountElem.innerText = mods.machete.inventory.toString()
-  macheteMultiplierElem.innerText = mods.machete.multiplier.toString()
 
-  toucanPriceElem.innerText = mods.toucan.price.toString()
-  toucanCountElem.innerText = mods.toucan.inventory.toString()
-  toucanMultiplierElem.innerText = mods.toucan.multiplier.toString()
 
-  monkeyPriceElem.innerText = mods.monkey.price.toString()
-  monkeyCountElem.innerText = mods.monkey.inventory.toString()
-  monkeyMultiplierElem.innerText = mods.monkey.multiplier.toString()
+  modElems.machete.machetePriceElem.innerText = mods.machete.price.toString()
+  modElems.machete.macheteCountElem.innerText = mods.machete.inventory.toString()
+  modElems.machete.macheteMultiplierElem.innerText = mods.machete.multiplier.toString()
 
-  troopPriceElem.innerText = mods.troop.price.toString()
-  troopCountElem.innerText = mods.troop.inventory.toString()
-  troopMultiplierElem.innerText = mods.troop.multiplier.toString()
+  modElems.toucan.toucanPriceElem.innerText = mods.toucan.price.toString()
+  modElems.toucan.toucanCountElem.innerText = mods.toucan.inventory.toString()
+  modElems.toucan.toucanMultiplierElem.innerText = mods.toucan.multiplier.toString()
 
+  modElems.monkey.monkeyPriceElem.innerText = mods.monkey.price.toString()
+  modElems.monkey.monkeyCountElem.innerText = mods.monkey.inventory.toString()
+  modElems.monkey.monkeyMultiplierElem.innerText = mods.monkey.multiplier.toString()
+
+  modElems.troop.troopPriceElem.innerText = mods.troop.price.toString()
+  modElems.troop.troopCountElem.innerText = mods.troop.inventory.toString()
+  modElems.troop.troopMultiplierElem.innerText = mods.troop.multiplier.toString()
 
   // console.log('draw coconutCount = ' + coconutCount)
   // console.log('... regularClick ' + 1)
@@ -215,5 +334,3 @@ let draw = () => {
 }
 
 draw()
-
-setCheckModAvailabilityInterval(100)
